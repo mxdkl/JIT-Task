b9e357c437bcfc2fce99c8ecf5ee7a2a3bd5702d
MxDkl <max@dekelnet.com>
2025-07-08 20:43:32 +0300

    comments/typing

--- src/scanner/audit.py
+++ src/scanner/audit.py
@@ -1,5 +1,7 @@
 import subprocess
 import json
+import os
+from typing import Optional, Dict, Any
 
 
 REPO_DIR = "/repo"
@@ -9,14 +11,14 @@ PACKAGE_LOCK = {}
 def init():
     # load package-lock.json file
     try:
-        with open(f"{REPO_DIR}/package-lock.json", 'r') as file:
+        with open(os.path.join(REPO_DIR, "package-lock.json"), 'r') as file:
             global PACKAGE_LOCK
             PACKAGE_LOCK = json.load(file)
     except FileNotFoundError:
         print("package-lock.json not found. Please ensure you are in a valid Node.js project directory.")
         return []
 
-def run_npm_audit() -> list:
+def run_npm_audit() -> Optional[Dict[str, Any]]:
     init()
     result = subprocess.run(
         ['npm', 'audit', '--json'],
@@ -27,14 +29,14 @@ def run_npm_audit() -> list:
     audit_json = _parse_audit_results(json.loads(result.stdout))
     return audit_json
 
-def _parse_audit_results(audit_json: dict) -> list:
+def _parse_audit_results(audit_json: dict) -> Optional[Dict[str, Any]]:
     vulnerabilities = {"results": []}
     
     for name, info in audit_json.get("vulnerabilities", {}).items():
         for vuln in info.get("via", []):
-            if isinstance(vuln, str):
+            if isinstance(vuln, str): # if the vulnerability is a string, vuln is not a direct vulnerability
                 continue
-            if vuln.get("name") != name:
+            if vuln.get("name") != name: # another check to ensure the vulnerability is in this package
                 continue
             for node in info.get("nodes", []):
                 version = _get_package_version(node)
@@ -50,12 +52,14 @@ def _parse_audit_results(audit_json: dict) -> list:
     return pretty_printed_vulnerabilities
 
 def _get_package_version(node: str) -> str:
+    # parese package-lock.json to get the version of the package
     if node in PACKAGE_LOCK.get("packages", {}):
         return PACKAGE_LOCK["packages"][node].get("version", "unknown")
 
     
 if __name__ == "__main__":
-    REPO_DIR = input("Repo path: ")
+    # for testing purposes
+    REPO_DIR = input("Full repo path: ")
     vulnerabilities = run_npm_audit()
     print(vulnerabilities)
 
